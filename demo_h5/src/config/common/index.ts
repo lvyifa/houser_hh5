0b928c99be0ae632dab70efa213e103f83027392
15100024890 <3026997707@qq.com>
2023-07-25 14:03:30 +0800

    456

--- demo_h5/src/config/common/index.ts
+++
@@ -1,3 +0,0 @@
-export const BASE_URL = process.env.VUE_APP_API_URL;
-// 直播接口请求前缀
-export const API_LIVE_PREFIX = process.env.VUE_APP_LIVE_URL_ADDRESS;
--- demo_h5/src/store/modules/app.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  namespaced: true,
-  name: "app",
-  state: {},
-  mutations: {},
-  actions: {},
-};
